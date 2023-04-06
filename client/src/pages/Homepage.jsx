f7ed7340db32afbf7df65ebe3adfb6a59abfe154
Auttasit.v <auttasit.v@gmail.com>
2023-04-06 10:28:25 +0700

    feat(homepage): merge page 1 and 2

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,32 +1,71 @@
+import image_1 from "../../public/header/image-1.png";
+import image from "../../public/header/image.png";
+import ellipse from "../../public/header/Ellipse 2.png";
+import smile from "../../public/header/smile.png";
+import ellipse_2 from "../../public/header/Ellipse 4.png";
+import vector_1 from "../../public/header/Vector (1).png";
+import vector from "../../public/header/Vector.png";
+
 function Homepage() {
   return (
-    <div className="part-2-container flex w-[1440px] h-[533px] bg-[#160404]">
-      <div className="text-container-outer flex flex-col justify-center items-center w-1/2 h-full pl-40">
-        <h1 className="text-[#DF89C6] pb-10 font-bold">Why Merry Match</h1>
-        <div className="text-container-inner flex flex-col gap-5">
-          <p className="text-white text-sm text-left px-4 font-sans">
-            Merry Match is a new generation of online dating website for
-            everyone.
-          </p>
-          <p className="text-white text-sm text-left px-4 font-sans ">
-            Whether you’re committed to dating, meeting new people, expanding
-            your social network, meeting locals while traveling, or even just
-            making a small chat with strangers.
-          </p>
-          <p className="text-white text-sm text-left px-4 font-sans ">
-            This site allows you to make your own dating profile, discover new
-            people, save favorite profiles, and let them know that you’re
-            interested.
-          </p>
+    <>
+      <body className="w-full h-screen bg-[#160404] relative flex justify-center items-center">
+        <img src={image_1} className="absolute left-40 bottom-0" />
+
+        <div className="flex flex-col justify-center items-center gap-10">
+          <h1 className="text-white text-6xl text-center">
+            Make the <br />
+            first ‘Merry’
+          </h1>
+          <h2 className="text-white text-center">
+            If you feel lonely, let’s start meeting
+            <br />
+            new people in your area!
+            <br />
+            Dont’t forget to get Merry with us
+          </h2>
+          <button className="bg-[#c70039] shadow-[2px_2px_12px_0_rgba(64, 50, 133, 0.16)] rounded-[99px] text-[#ffffff] h-[48px] w-[163px] mt-[5%] font-[700] hover:bg-[#FF1659]">
+            Start matching!
+          </button>
+        </div>
+
+        <img src={image} className="absolute right-40 -top-20" />
+        <img src={ellipse} className="absolute right-20 top-[430px]" />
+        <img src={ellipse} className="absolute -left-3 top-24" />
+        <img src={smile} className="absolute right-32 top-[460px]" />
+        <img src={ellipse_2} className="absolute right-52 bottom-36" />
+        <img src={ellipse_2} className="absolute left-32 top-20" />
+        <img src={vector_1} className="absolute right-[600px] top-36" />
+        <img src={vector} className="absolute left-[170px] top-[273px]" />
+      </body>
+      <div className="part-2-container flex w-full h-[533px] bg-[#160404]">
+        <div className="text-container-outer flex flex-col justify-center items-center w-1/2 h-full pl-40">
+          <h1 className="text-[#DF89C6] pb-10 font-bold">Why Merry Match</h1>
+          <div className="text-container-inner flex flex-col gap-5">
+            <p className="text-white text-sm text-left px-4 font-sans">
+              Merry Match is a new generation of online dating website for
+              everyone.
+            </p>
+            <p className="text-white text-sm text-left px-4 font-sans ">
+              Whether you’re committed to dating, meeting new people, expanding
+              your social network, meeting locals while traveling, or even just
+              making a small chat with strangers.
+            </p>
+            <p className="text-white text-sm text-left px-4 font-sans ">
+              This site allows you to make your own dating profile, discover new
+              people, save favorite profiles, and let them know that you’re
+              interested.
+            </p>
+          </div>
+        </div>
+        <div className="picture container flex flex-col justify-center items-center w-1/2 h-full">
+          <img
+            className="h-[348px] w-[546px]"
+            src="../src/assets/fast-and-easy.png"
+          />
         </div>
       </div>
-      <div className="picture container flex flex-col justify-center items-center w-1/2 h-full">
-        <img
-          className="h-[348px] w-[546px]"
-          src="../src/assets/fast-and-easy.png"
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
