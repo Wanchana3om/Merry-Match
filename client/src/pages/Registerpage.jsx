9a4fdc8bb9f247f8887ce0dcd3dcc5c4dc42cc66
Gusbell <Gusbell@users.noreply.github.com>
2023-04-07 11:10:42 +0700

    feat: change form when next pages

--- client/src/pages/Registerpage.jsx
+++ client/src/pages/Registerpage.jsx
@@ -1,7 +1,25 @@
 import Navigationbar from "../components/Navigationbar";
 import RegisterForm1 from "../components/RegisterForm1";
+import RegisterForm2 from "../components/RegisterForm2";
+import { useState } from "react";
 
 function Registerpage() {
+  const [currentFormPage, setCurrentFormPage] = useState(1);
+  const handleNextStep = () => {
+    if (currentFormPage === 3) {
+      return null;
+    } else {
+      setCurrentFormPage(currentFormPage + 1);
+    }
+  };
+  const handleBack = () => {
+    if (currentFormPage != 1) {
+      setCurrentFormPage(currentFormPage - 1);
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div>
       <Navigationbar />
@@ -16,55 +34,97 @@ function Registerpage() {
           </h2>
         </div>
         <div className="tabs-container rounded-2xl tabs-container flex gap-4 w-1/2 h-full justify-items-center items-center">
-          <div className="border-4 border-[#E4E6ED] px-[16px] w-auto h-[80px] rounded-[16px] flex justify-evenly items-center">
-            <div className="w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row justify-center items-center text-[#A62D82] font-bold text-[24px]">
+          <div
+            className={`border-4 border-[#E4E6ED] px-[16px] ${
+              currentFormPage === 1 ? "w-auto" : "w-[80px]"
+            } h-[80px] rounded-[16px] flex justify-evenly items-center`}
+          >
+            <div
+              className={`w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row justify-center items-center font-bold text-[24px] ${
+                currentFormPage === 1 ? "text-[#A62D82]" : "text-[#9AA1B9]"
+              }`}
+            >
               1
             </div>
-            <div>
+            <div className={`${currentFormPage > 1 ? "hidden" : ""}`}>
               <div className="text-[12px] px-[16px] font-medium text-[#646D89]">
-                Step 1/3
+                Step {currentFormPage}/3
               </div>
               <div className="text-base px-[16px] font-extrabold text-[#A62D82]">
                 Basic Information
               </div>
             </div>
           </div>
-          <div className="border-4 border-[#E4E6ED] w-[80px] h-[80px] rounded-[16px] flex flex-row justify-center items-center ">
-            <div className="w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row justify-center items-center text-[#9AA1B9] font-bold text-[24px]">
+          <div
+            className={`border-4 border-[#E4E6ED] h-[80px] rounded-[16px] flex flex-row justify-center items-center ${
+              currentFormPage === 2 ? "w-auto px-3" : "w-[80px]"
+            }`}
+          >
+            <div
+              className={`w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row justify-center items-center text-[#9AA1B9] font-bold text-[24px] ${
+                currentFormPage === 2 ? "text-[#A62D82]" : "text-[#9AA1B9]"
+              }`}
+            >
               2
             </div>
+            <div className={`${currentFormPage != 2 ? "hidden" : ""}`}>
+              <div className="text-[12px] px-[16px] font-medium text-[#646D89]">
+                Step {currentFormPage}/3
+              </div>
+              <div className="text-base px-[16px] font-extrabold text-[#A62D82]">
+                Identities and Interests
+              </div>
+            </div>
           </div>
-          <div className="border-4 border-[#E4E6ED] w-[80px] h-[80px] rounded-[16px] flex flex-row justify-center items-center">
-            <div className="w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row items-center  justify-center text-[#9AA1B9] font-bold text-[24px]">
+          <div
+            className={`border-4 border-[#E4E6ED] w-[80px] h-[80px] rounded-[16px] flex flex-row justify-center items-center ${
+              currentFormPage === 3 ? "w-auto px-3" : "w-[80px]"
+            }`}
+          >
+            <div
+              className={`w-[48px] h-[48px] rounded-[16px] bg-[#F1F2F6] flex flex-row items-center  justify-center text-[#9AA1B9] font-bold text-[24px] ${
+                currentFormPage === 3 ? "text-[#A62D82]" : "text-[#9AA1B9]"
+              }`}
+            >
               3
+            </div>
+            <div className={`${currentFormPage != 3 ? "hidden" : ""}`}>
+              <div className="text-[12px] px-[16px] font-medium text-[#646D89]">
+                Step {currentFormPage}/3
+              </div>
+              <div className="text-base px-[16px] font-extrabold text-[#A62D82]">
+                Upload Photos
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <RegisterForm1 />
-      {/* <div className="info-container">
-        <div className="info-container-left"></div>
-      </div> */}
+      {currentFormPage === 1 && <RegisterForm1 />}
+      {currentFormPage === 2 && <RegisterForm2 />}
+      {/* {currentFormPage === 3 && <RegisterForm3 />} */}
       <div className="relative z-30 w-full border-t-2">
         <div className="w-[77%] flex flex-row justify-between items-center h-auto py-5 bg-white mx-auto ">
           <nav className="">
             <a href="#" className=" font-[400] text-base text-[#646D89]">
-              1/3
+              {currentFormPage}/3
             </a>
           </nav>
           <nav>
             <div className="flex flex-row mx-3">
               <div className="m-3 text-base font-bold hover:text-[#191C77]">
                 <img src="/Vector.png" alt="" />
-                <a
-                  href="#"
-                  class="text-[#C8CCDB] font-semibold mr-[17px] font-weight: bold; hover:text-[#191C77]"
+                <button
+                  onClick={handleBack}
+                  className="text-[#C8CCDB] font-semibold mr-[17px] font-weight: bold; hover:text-[#191C77]"
                 >
                   Back
-                </a>
+                </button>
               </div>
-              <button class="bg-red-500 hover:bg-red-600 text-white font-bold  px-6 rounded-full">
-                Next step
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white font-bold  px-6 rounded-full"
+                onClick={handleNextStep}
+              >
+                {currentFormPage === 3 ? 'Confirm' : 'Next step'}
               </button>
             </div>
           </nav>
