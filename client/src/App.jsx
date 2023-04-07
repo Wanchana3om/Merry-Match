847233498adbab55917fc76ed97dff01e360fd11
Auttasit.v <auttasit.v@gmail.com>
2023-04-07 15:12:30 +0700

    fix: remove unnecessary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,11 @@ import Navigationbar from "./components/Navigationbar";
 import Footer from "./components/Footer";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
-import DraggableList from "./components/DraggableItem";
 
 function App() {
   return (
     <>
       <Registerpage />
-      <DraggableList />
       {/* <Navigationbar />
       <Homepage />
       <Footer /> */}
--- client/src/components/RegisterForm3.jsx
+++ client/src/components/RegisterForm3.jsx
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
-function RegisterForm3() {
-=======
-import { useState } from 'react';
+import { useState } from "react";
 
 function RegisterForm3() {
-  const [images, setImages] = useState([null, null, null, null]);
+  const [images, setImages] = useState([null, null, null, null, null]);
 
   const handleImageClick = (index) => {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*';
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
     input.onchange = (event) => {
       const file = event.target.files[0];
       const reader = new FileReader();
@@ -27,8 +24,8 @@ function RegisterForm3() {
 
   const handleImageDrop = (event, index) => {
     event.preventDefault();
-    const droppedIndex = event.dataTransfer.getData('text');
-    if (droppedIndex === '') return;
+    const droppedIndex = event.dataTransfer.getData("text");
+    if (droppedIndex === "") return;
     setImages((prevImages) => {
       const newImages = [...prevImages];
       const temp = newImages[index];
@@ -39,14 +36,13 @@ function RegisterForm3() {
   };
 
   const handleDragStart = (event, index) => {
-    event.dataTransfer.setData('text', index);
+    event.dataTransfer.setData("text", index);
   };
 
   const handleDragOver = (event) => {
     event.preventDefault();
   };
 
->>>>>>> 0c8e90ac51dd7cf17127567ba15574cfa2c6e3b9
   return (
     <div className="form-container px-[255px] mb-24 py-8 h-[500px]">
       <h1 className="text-2xl text-[#A62D82] font-[700] mb-[24px]">
@@ -55,14 +51,6 @@ function RegisterForm3() {
       <h2>Upload at least 2 photos</h2>
 
       <div className="grid grid-cols-5 grid-rows-1 gap-2">
-<<<<<<< HEAD
-        <div className="w-[167px] h-[167px] bg-red-200 rounded-2xl">22</div>
-        <div className="w-[167px] h-[167px] bg-red-200 rounded-2xl">22</div>
-        <div className="w-[167px] h-[167px] bg-red-200 rounded-2xl">qd</div>
-        <div className="w-[167px] h-[167px] bg-red-200 rounded-2xl">dqead</div>
-        <input type="file" className="w-[167px] h-[167px] bg-red-200" />
-        wesa
-=======
         {images.map((image, index) => (
           <div
             key={index}
@@ -74,13 +62,12 @@ function RegisterForm3() {
             onDragStart={(event) => handleDragStart(event, index)}
             style={{
               backgroundImage: `url(${image})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'contain',
-              backgroundPosition: 'center',
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
             }}
           />
         ))}
->>>>>>> 0c8e90ac51dd7cf17127567ba15574cfa2c6e3b9
       </div>
     </div>
   );
