4ddc780ae4a03337e9d3e355e79aa1fb024c0cef
Gusbell <Gusbell@users.noreply.github.com>
2023-04-10 11:54:35 +0700

    fix: deleted test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
-import App from "./test";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
