279ecffebae02d39d993c89359acba246acff733
yotsatorn <yotsatorn.pa@gmail.com>
2023-04-10 17:31:08 +0700

    feat: add axios

--- client/src/components/Navigationbar.jsx
+++ client/src/components/Navigationbar.jsx
@@ -2,44 +2,40 @@ import { Link } from "react-router-dom";
 
 function Navigationbar() {
 
-  
-
-  const handleOnClick = (e, id, path) => {
+  const handleOnClick = (e, id) => {
     e.preventDefault();
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: "smooth" });
-    
   };
 
   return (
     <div className="relative z-30 w-full shadow-md">
       <div className="w-[77%]  flex flex-row justify-between items-center py-5 bg-white mx-auto ">
         <nav>
-          <a href="#" className="text-black font-semibold text-4xl">
+          <Link to="/" className="text-black font-semibold text-4xl">
             Merry
-          </a>
-          <a href="#" className="text-red-500 font-bold text-4xl">
+          </Link>
+          <Link to="/" className="text-red-500 font-bold text-4xl">
             Match
-          </a>
+          </Link>
         </nav>
         <nav>
           <ul className="flex flex-row items-center">
             <li className="mr-[56px] text-base font-bold hover:text-[#191C77]">
-              <a
-                href="http://localhost:5173/"
-                onClick={(e) => handleOnClick(e, "why-merry")}
-              >
+              <Link to="/" onClick={(e) => handleOnClick(e, "why-merry")}>
                 Why Merry Match?
-              </a>
+              </Link>
             </li>
             <li className="mr-[56px] text-base font-bold hover:text-[#191C77]">
-              <Link to="/" href="http://localhost:5173/" onClick={(e) => handleOnClick(e, "how-to", "/")}>
+              <Link to="/" onClick={(e) => handleOnClick(e, "how-to")}>
                 How to Merry
               </Link>
             </li>
-            <Link to="/login"><button className="bg-red-500 hover:bg-red-600 text-white font-bold mx-5 px-6 py-2 rounded-full h-[66] ">
-              Login
-            </button></Link>
+            <Link to="/login">
+              <button className="bg-red-500 hover:bg-red-600 text-white font-bold mx-5 px-6 py-2 rounded-full h-[66] ">
+                Login
+              </button>
+            </Link>
           </ul>
         </nav>
       </div>
