d3aa3c100fd925ddc69c028e4ce0fc3806ae4716
Auttasit.v <auttasit.v@gmail.com>
2023-04-21 13:24:05 +0700

    fix: fix matching page rendering

--- client/src/pages/MatchingPage.jsx
+++ client/src/pages/MatchingPage.jsx
@@ -1,105 +1,68 @@
 import NavigationbarUser from "../components/NavigationbarUser";
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo, useRef, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import ProfilePopup from "../components/ProfilePopup";
 import eye_button from "/merrylist/eye_button.png";
-import useData from "../hook/useData";
-import {
-  RangeSlider,
-  RangeSliderTrack,
-  RangeSliderFilledTrack,
-  RangeSliderThumb,
-} from "@chakra-ui/react";
-
-const db = [
-  {
-    name: "Richard Hendricks",
-    url: "/img/richard.jpg",
-  },
-  {
-    name: "Erlich Bachman",
-    url: "/img/erlich.jpg",
-  },
-  {
-    name: "Monica Hall",
-    url: "/emmy.jpg",
-  },
-  {
-    name: "Jared Dunn",
-    url: "/jbareham.jpg",
-  },
-  {
-    name: "Dinesh Chugtai",
-    url: "/dragon.jpg",
-  },
-];
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 function MatchingPage() {
-  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
-  const [lastDirection, setLastDirection] = useState();
-  const [keyword, setKeyword] = useState("");
-  const [meetingInterest, setMeetingInterest] = useState([]);
-  const [minAge, setMinAge] = useState(18);
-  const [maxAge, setMaxAge] = useState(50);
-  // const [range, setRange] = useState([18, 50]);
+  const [matchingList, setMatchingList] = useState([]);
 
-  const { getData } = useData();
+  const restructureData = (data) => {
+    const usersMap = new Map();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    getData({
-      keyword,
-      meetingInterest,
-      minAge,
-      maxAge,
+    data.forEach((item) => {
+      const user = item.users;
+      if (!usersMap.has(user.user_id)) {
+        usersMap.set(user.user_id, user);
+      }
     });
+
+    return Array.from(usersMap.values());
   };
 
-  const handleCheckboxChange = (event) => {
-    const value = event.target.value;
+  const getMatchingProfile = async () => {
+    const token = localStorage.getItem("token");
 
-    if (event.target.checked) {
-      setMeetingInterest((prevMeetingInterest) => [
-        ...prevMeetingInterest,
-        value,
-      ]);
-    } else {
-      setMeetingInterest((prevMeetingInterest) =>
-        prevMeetingInterest.filter((item) => item !== value)
-      );
+    if (token) {
+      try {
+        const userDataFromToken = jwtDecode(token);
+
+        const result = await axios.get(`http://localhost:3000/users/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        let matchingData = result.data;
+        const newMatchingList = restructureData(matchingData);
+        console.log(result.data);
+        setMatchingList(newMatchingList);
+      } catch (error) {
+        console.error("Error decoding the token or fetching user data:", error);
+      }
     }
   };
-  console.log(keyword);
-  console.log(meetingInterest);
-  console.log(minAge);
-  console.log(maxAge);
 
-  const handleRangeChange = (newRange) => {
-    setMinAge(newRange[0]);
-    setMaxAge(newRange[1]);
-  };
+  useEffect(() => {
+    getMatchingProfile();
+  }, []);
 
-  const handleMinChange = (event) => {
-    setMinAge(parseInt(event.target.value));
-  };
+  useEffect(() => {
+    console.log(matchingList);
+  }, [matchingList]);
 
-  const handleMaxChange = (event) => {
-    setMaxAge(parseInt(event.target.value));
-  };
+  const [ageRange, setAgeRange] = useState(18);
 
-  const handleClear = (event) => {
-    event.preventDefault();
-    setKeyword("");
-    setMeetingInterest([]);
-    window.location.reload();
-  };
+  const [currentIndex, setCurrentIndex] = useState(matchingList.length - 1);
+  const [lastDirection, setLastDirection] = useState();
 
   const currentIndexRef = useRef(currentIndex);
 
   const childRefs = useMemo(
     () =>
-      Array(db.length)
+      Array(matchingList.length)
         .fill(0)
         .map((i) => React.createRef()),
     []
@@ -110,7 +73,7 @@ function MatchingPage() {
     currentIndexRef.current = val;
   };
 
-  const canGoBack = currentIndex < db.length - 1;
+  const canGoBack = currentIndex < matchingList.length - 1;
 
   const canSwipe = currentIndex >= 0;
 
@@ -125,7 +88,7 @@ function MatchingPage() {
   };
 
   const swipe = async (dir) => {
-    if (canSwipe && currentIndex < db.length) {
+    if (canSwipe && currentIndex < matchingList.length) {
       await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
     }
   };
@@ -138,7 +101,9 @@ function MatchingPage() {
     await childRefs[newIndex].current.restoreCard();
   };
 
-  
+  const handleAgeRangeChange = (event) => {
+    setAgeRange(event.target.value);
+  };
   const [showProfile, setShowProfile] = useState(false);
   const handleShowProfile = () => {
     setShowProfile(!showProfile);
@@ -201,21 +166,23 @@ function MatchingPage() {
         </div>
 
         {/* ------------------------section 2 ----------------------------  */}
-        <div className="bg-[#160404] col-span-3 w-[904px] overflow-hidden">
-          <div className="relative w-[620px] h-[620px] mt-40 rounded-[32px]">
-            {db.map((character, index) => (
+        <div className="bg-gray-300 col-span-3 w-[904px] overflow-hidden">
+          <div className="relative w-[620px] h-[620px] rounded-[32px]">
+            {matchingList.map((item, index) => (
               <TinderCard
                 ref={childRefs[index]}
                 className="absolute top-0 left-32 w-full h-full rounded-[32px] bg-gradient-to-t from-[#390741] to-[#070941]"
-                key={character.name}
-                onSwipe={(dir) => swiped(dir, character.name, index)}
-                onCardLeftScreen={() => outOfFrame(character.name, index)}
+                key={item.user_id}
+                onSwipe={(dir) => swiped(dir, item.name, index)}
+                onCardLeftScreen={() => outOfFrame(item.name, index)}
               >
                 <div
-                  style={{ backgroundImage: "url(" + character.url + ")" }}
+                  style={{
+                    backgroundImage: "url(" + item.pictures[0].pic_url + ")",
+                  }}
                   className="card h-full w-full flex items-end px-4 py-3 text-white rounded-[32px] bg-gradient-to-t from-[#390741] to-[#070941]"
                 >
-                  <h3>{character.name}</h3>
+                  <h3>{item.name}</h3>
                   <div>
                     <button onClick={handleShowProfile}>
                       <img
@@ -243,148 +210,105 @@ function MatchingPage() {
             />
           </div>
         </div>
-
         {/* ------------------------section 3 ----------------------------  */}
-        <form
-          onSubmit={handleSubmit}
-          className="   w-[210px] flex flex-row justify-center "
-        >
+
+        <div className="   w-[220px] flex flex-row justify-center ">
           <div className=" flex flex-col items-center  w-[188px] mx-auto>">
             <div className="flex flex-col gap-10 mb-[170px]">
               <div className="mt-6 flex flex-col gap-5">
                 <h1 className="text-[#191C77] font-bold">Search by Keywords</h1>
                 <input
-                  placeholder="Search..."
+                  placeholder="   Search..."
                   type="text"
-                  value={keyword}
-                  className="py-3 border-[1px] w-[168px] px-[10px] border-[#CCD0D7] rounded-lg "
-                  onChange={(event) => {
-                    setKeyword(event.target.value);
-                  }}
+                  className="py-3 border-[1px] border-[#CCD0D7] rounded-lg"
                 />
               </div>
-
-              <div className="flex flex-col gap-3 ">
-                <h1 className="text-[#191C77] font-bold">Sex you interest</h1>
-                <div className="flex">
+              <div class="flex flex-col gap-3 ">
+                <h1 class="text-[#191C77] font-bold">Sex you interest</h1>
+                <div class="flex">
                   <input
                     type="checkbox"
-                    id="Friends"
-                    name="Friends"
-                    value="Friends"
-                    className="w-[24px] h-[24px] rounded-lg"
-                    onChange={handleCheckboxChange}
+                    id="sex1"
+                    name="sex1"
+                    value="Default"
+                    class="w-[24px] h-[24px] rounded-lg"
                   />
-                  <label htmlFor="sex1" className="ml-[12px] text-[#646D89]">
-                    Friends
+                  <label for="sex1" class="ml-[12px] text-[#646D89]">
+                    Default
                   </label>
                 </div>
-                <div className="flex mt-[16px]">
+                <div class="flex mt-[16px]">
                   <input
                     type="checkbox"
-                    id="Partners"
-                    name="Partners"
-                    value="Partners"
-                    className="w-[24px] h-[24px] rounded-lg"
-                    onChange={handleCheckboxChange}
+                    id="sex2"
+                    name="sex2"
+                    value="Female"
+                    class="w-[24px] h-[24px] rounded-lg"
                   />
-
-                  <label htmlFor="sex2" className="ml-[12px] text-[#646D89]">
-                    Partners
+                  <label for="sex2" class="ml-[12px] text-[#646D89]">
+                    Female
                   </label>
                 </div>
-                <div className="flex mt-[16px]">
+                <div class="flex mt-[16px]">
                   <input
                     type="checkbox"
-                    id="Short-term commitment"
-                    name="Short-term commitment"
-                    value="Short-term commitment"
-                    className="w-[24px] h-[24px] rounded-lg"
-                    onChange={handleCheckboxChange}
+                    id="sex3"
+                    name="sex3"
+                    value="Non-binary people"
+                    class="w-[24px] h-[24px] rounded-lg"
                   />
-
-                  <label htmlFor="sex3" className="ml-[12px] text-[#646D89]">
-                    Short-term
-                  </label>
-                </div>
-                <div className="flex mt-[16px]">
-                  <input
-                    type="checkbox"
-                    id="Long-term commitment"
-                    name="Long-term commitment"
-                    value="Long-term commitment"
-                    className="w-[24px] h-[24px] rounded-lg"
-                    onChange={handleCheckboxChange}
-                  />
-
-                  <label htmlFor="sex3" className="ml-[12px] text-[#646D89]">
-                    Long-term
+                  <label for="sex3" class="ml-[12px] text-[#646D89]">
+                    Non-binary people
                   </label>
                 </div>
               </div>
 
               <div className="flex flex-col gap-6">
-                <label htmlFor="age-range" className="text-blue-700 font-bold">
+                <label for="age-range" className="text-[#191C77] font-bold">
                   Age Range
                 </label>
-                <div className="relative w-full">
-                  <RangeSlider
-                    aria-label={["min", "max"]}
-                    colorScheme="pink"
-                    defaultValue={[minAge, maxAge]}
-                    min={18}
-                    max={100}
-                    onChange={handleRangeChange}
-                    >
-                    <RangeSliderTrack>
-                      <RangeSliderFilledTrack />
-                    </RangeSliderTrack>
-                    <RangeSliderThumb index={0} />
-                    <RangeSliderThumb index={1} />
-                  </RangeSlider>
-                  <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-between mt-[40px]">
-                    <input
-                      type="number"
-                      id="min"
-                      value={minAge}
-                      onChange={handleMinChange}
-                      min={18}
-                      max={maxAge}
-                      className="border-[1px] mr-[4px] border-[#D6D9E4] py-3 pr-4 pl-3 w-[85.5px] h-[48px] rounded-lg"
-                      />
-                    <div className="mt-[10px]"> - </div>
-                    <input
-                      type="number"
-                      id="max"
-                      value={maxAge}
-                      onChange={handleMaxChange}
-                      min={minAge}
-                      max={100}
-                      className="border-[1px] ml-[4px] border-[#D6D9E4] py-3 pr-4 pl-3 w-[85.5px] h-[48px] rounded-lg"
-                      />
-                  </div>
+                <input
+                  type="range"
+                  id="age-range"
+                  name="age-range"
+                  min="18"
+                  max="100"
+                  value={ageRange}
+                  className="block w-full h-1 mt-1 bg-gray-300 rounded-md appearance-none focus:outline-none"
+                  onInput={handleAgeRangeChange}
+                />
+                <div className="flex justify-evenly items-center">
+                  <input
+                    type="number"
+                    id="min-age"
+                    name="min-age"
+                    value={ageRange}
+                    className="border-[1px] border-[#D6D9E4] py-3 pr-4 pl-3 w-[85.5px] h-[48px] rounded-lg"
+                  />
+                  <p> - </p>
+                  <input
+                    type="number"
+                    id="max-age"
+                    name="max-age"
+                    value="100"
+                    className="border-[1px] border-[#D6D9E4] py-3 pr-4 pl-3 w-[85.5px] h-[48px] rounded-lg"
+                  />
                 </div>
               </div>
             </div>
 
-            <div className="w-[210px]  flex justify-center items-center border-t-[1px] border-gray-400">
+            <div className="w-[220px] flex justify-center items-center border-t-[1px] border-gray-400">
               <div className="flex justify-center items-center w-[188px]  pt-6 ">
-                <h1
-                  onClick={handleClear}
-                  className="py-3 px-6 text-[#C70039] hover:underline hover:text-[#FF1659] hover:cursor-pointer"
-                >
+                <h1 className="py-3 px-6 text-[#C70039] hover:underline hover:text-[#FF1659] hover:cursor-pointer">
                   Clear
                 </h1>
-                <button
-                  type="submit"
-                  className="py-3 px-6 bg-[#C70039] rounded-[99px] text-white hover:bg-[#FF1659]"
-                >
+                <button className="py-3 px-6 bg-[#C70039] rounded-[99px] text-white hover:bg-[#FF1659]">
                   Search
                 </button>
               </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
