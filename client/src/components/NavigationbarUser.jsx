b3da383e3220a95e03527dd340b7acec9b4c71e3
Wanchana Inmasom <122344268+Wanchana3om@users.noreply.github.com>
2023-04-28 11:22:22 +0700

    fix: debug  profile toggle

--- client/src/components/NavigationbarUser.jsx
+++ client/src/components/NavigationbarUser.jsx
@@ -1,5 +1,17 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/authentication";
+import React, { useState } from "react";
+import { Flex, useDisclosure } from "@chakra-ui/react";
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Image,
+  MenuDivider,
+} from "@chakra-ui/react";
 
 function NavigationbarUser() {
   // const [showMenu, setShowMenu] = useState(false);
@@ -7,7 +19,7 @@ function NavigationbarUser() {
   // const toggleMenu = () => {
   //   setShowMenu(!showMenu);
   // };
-
+  // const { isOpen, onToggle } = useDisclosure()
   const { logout, state } = useAuth();
 
   return (
@@ -29,94 +41,95 @@ function NavigationbarUser() {
             <li className="mr-[56px] text-base font-bold hover:text-[#191C77] ">
               <Link to="/merrypackage">Merry Membership</Link>
             </li>
-
-            <div className="pr-[12px]">
+            <div className="">
               <img
                 src="/nav-bar/bell.svg"
                 alt="Profile"
                 className="w-[48px] h-[48px] rounded-full cursor-pointer"
               />
             </div>
-            <button className="group ">
-              <div className="relative mr-[16px]">
-                <img
-                  src={state?.user?.profilePic}
-                  alt="Profile"
-                  className="w-[48px] h-[48px]  rounded-full cursor-pointer object-cover hover:scale-125 transition-all duration-300"
-                />
-              </div>
-              <div className="font-nunito  absolute z-50 w-[204px] mt-2 bg-white rounded-[12px] shadow-lg flex flex-col p-[8px] opacity-0 group-focus:opacity-100 transition-all duration-700">
-                <img src="/nav-bar/premium cta.svg" alt="icon" />
-                <div className="flex items-center p-[8px]  hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
-                  <img
-                    src="/nav-bar/Vector.svg"
-                    className="w-[13px] h-[13px] "
-                    alt=""
+
+            <Menu width="204px">
+              <MenuButton
+                as={Button}
+                colorScheme="white"
+                _hover={{ scale: 125 }}
+              >
+                <div>
+                  <Image
+                    src={state?.user?.profilePic}
+                    alt="Profile"
+                    className="w-[48px] h-[48px] rounded-full cursor-pointer object-cover transition-all duration-300 hover:scale-125"
                   />
-                  <a
-                    href="/ownerprofile"
-                    className="block px-4 py-2 text-sm text-gray-700 "
-                  >
-                    Profile
-                  </a>
                 </div>
-                <div className="flex items-center p-[8px] hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
-                  <img
-                    src="/nav-bar/Vector2.png"
-                    className="w-[13px] h-[13px] "
-                    alt="icon"
-                  />
-                  <a
-                    href="/merrylist"
-                    className="block px-4 py-2 text-sm text-gray-700 "
-                  >
-                    Merry list
-                  </a>
-                </div>
-                <div className="flex items-center p-[8px]  hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px] w-full">
-                  <img
-                    src="/nav-bar/Vector (2).svg"
-                    className="w-[13px] h-[13px] "
-                    alt="icon"
-                  />
-                  <a
-                    href="/merrypackage"
-                    className="block px-4 py-2 text-sm text-gray-700 "
-                  >
-                    Merry Membership
-                  </a>
-                </div>
-                <div className="flex items-center p-[8px] hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
-                  <img
-                    src="/nav-bar/Vector (3).svg"
-                    className="w-[13px] h-[13px] "
-                    alt="icon"
-                  />
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 "
-                  >
-                    Complaint
-                  </a>
-                </div>
-                <div className="flex items-center p-[8px] border-t-[1px] border-gray hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
-                  <img
-                    src="/nav-bar/Vector (4).svg"
-                    className="w-[13px] h-[13px] "
-                    alt="icon"
-                  />
-                  <a
-                    href="/"
-                    className="block px-4 py-2 text-sm text-gray-700 "
-                    onClick={() => {
-                      logout();
-                    }}
-                  >
-                    Log Out
-                  </a>
-                </div>
-              </div>
-            </button>
+              </MenuButton>
+
+              <MenuList borderRadius={12} width="204px" alignItems="center">
+                <MenuItem _hover={{ borderRadius: 12 }} alignItems="center">
+                  <div className="flex items-center px-[8px]">
+                    <img
+                      src="/nav-bar/Vector.svg"
+                      className="w-[13px] h-[13px] "
+                      alt=""
+                    />
+                    <a
+                      href="/ownerprofile"
+                      className="block px-4 py-2  text-gray-700 "
+                    >
+                      Profile
+                    </a>
+                  </div>
+                </MenuItem>
+                <MenuItem _hover={{ borderRadius: 12 }}>
+                  <div className="flex items-center px-[8px] hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
+                    <img
+                      src="/nav-bar/Vector2.png"
+                      className="w-[13px] h-[13px] "
+                      alt="icon"
+                    />
+                    <a
+                      href="/merrylist"
+                      className="block px-4 py-2  text-gray-700 "
+                    >
+                      Merry list
+                    </a>
+                  </div>
+                </MenuItem>
+
+                <MenuItem _hover={{ borderRadius: 12 }}>
+                  <div className="flex items-center px-[8px] hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
+                    <img
+                      src="/nav-bar/Vector (3).svg"
+                      className="w-[13px] h-[13px] "
+                      alt="icon"
+                    />
+                    <a href="#" className="block px-4 py-2  text-gray-700 ">
+                      Complaint
+                    </a>
+                  </div>
+                </MenuItem>
+
+                <MenuDivider />
+                <MenuItem _hover={{ borderRadius: 12 }}>
+                  <div className="flex items-center px-[8px] border-gray hover:bg-gray-100 hover:cursor-pointer hover:rounded-[12px]">
+                    <img
+                      src="/nav-bar/Vector (4).svg"
+                      className="w-[13px] h-[13px] "
+                      alt="icon"
+                    />
+                    <a
+                      href="/"
+                      className="block px-4 py-2  text-gray-700 "
+                      onClick={() => {
+                        logout();
+                      }}
+                    >
+                      Log Out
+                    </a>
+                  </div>
+                </MenuItem>
+              </MenuList>
+            </Menu>
           </ul>
         </nav>
       </div>
